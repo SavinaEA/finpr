b2f425526d6834cd9612c034b15ef08f7011357f
SavinaEA <selena06065@gmai.com>
2023-03-10 22:48:13 +0700

    decision third point

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ string[] CopyMyArray(string[] firstArray)
 }
 
 //2.  Определение количества элементов массива по требуемому условию
-int CalkWoldArray(string[] inArray,int k)
+int CalkWoldArray(string[] inArray, int k)
 {
     int countW0ld = 0;
     string wold = "";
@@ -25,16 +25,40 @@ int CalkWoldArray(string[] inArray,int k)
     return countW0ld;
 }
 
+//3. Создание нового массива по количеству элементов
+string[] CreateSecondArray(string[] firstArray, int size, int k)
+{
+    string[] secondArray = new string[size];
+    string wold = "";
+    int j = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        wold = firstArray[i];
+        int countLetter = wold.Length;
+        if (countLetter <= k)
+        {
+            secondArray[j] = wold;
+            j++;
+        }
+    }
+    return secondArray;
+}
+
 Console.Clear();
 string[] myArray = { "Hello", "2", "World", ":-)" };
 int K = 3;
-Console.WriteLine($"\nЗадание: Выбрать из списка элементы их {K}-х символов и менее ");
+Console.WriteLine($"Задание: Выбрать из списка элементы их {K}-х символов и менее ");
 Console.Write("\nВходящий список: ");
-string[] myArray1 = CopyMyArray(myArray);
+string[] myArray1 =  CopyMyArray(myArray);
 Console.Write(String.Join(", ", myArray1));
 Console.WriteLine();
 Console.WriteLine($"\nЭлементов в первичном наборе: {myArray1.Length}");
-Console.WriteLine();
 
 int calk = CalkWoldArray(myArray1, K);
+string[] myArray2 = CreateSecondArray(myArray1, calk, K);
+Console.WriteLine();
 Console.WriteLine($"Элементов в итоговом наборе: {calk}");
+
+Console.Write("\nИтоговый список: ");
+Console.WriteLine(String.Join(", ", myArray2));
+Console.WriteLine();
