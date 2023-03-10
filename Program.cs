b7925da57dd30bd0dc18c91910bd84c6360ce275
SavinaEA <selena06065@gmai.com>
2023-03-10 23:44:53 +0700

    create new list, change vvod in massiv entry and Random find count element

--- Program.cs
+++ Program.cs
@@ -44,20 +44,42 @@ string[] CreateSecondArray(string[] firstArray, int size, int k)
     return secondArray;
 }
 
+//4. Добавление новыхэлементов в массив
+string[] Append(string[] array, int count)
+{
+    string[] result = new string[array.Length + count];
+    array.CopyTo(result, 0);
+    for (int i = array.Length; i < result.Length; i++)
+    {
+        Console.Write("\nВведите новый элемент: ");
+        string text = Console.ReadLine()!;
+        result[i] = text;
+    }
+    return result;
+}
+
+
+
 Console.Clear();
-string[] myArray = { "Hello", "2", "World", ":-)" };
-int K = 3;
-Console.WriteLine($"Задание: Выбрать из списка элементы их {K}-х символов и менее ");
+string[] myArray = { "Hello", "World", "!" };
+int K = new Random().Next(1, 6
+);;
+Console.WriteLine($"Задание: Выбор элементов из списка");
 Console.Write("\nВходящий список: ");
-string[] myArray1 =  CopyMyArray(myArray);
+string[] myArray1 = CopyMyArray(myArray);
 Console.Write(String.Join(", ", myArray1));
 Console.WriteLine();
 Console.WriteLine($"\nЭлементов в первичном наборе: {myArray1.Length}");
 
-int calk = CalkWoldArray(myArray1, K);
-string[] myArray2 = CreateSecondArray(myArray1, calk, K);
+Console.Write("\nДавайте добавим элементов в программный список. Сколько?: ");
+int N = int.Parse(Console.ReadLine()!);
+string[] result = Append(myArray1, N);
+
+int calk = CalkWoldArray(result, K);
+string[] myArray2 = CreateSecondArray(result, calk, K);
 Console.WriteLine();
-Console.WriteLine($"Элементов в итоговом наборе: {calk}");
+Console.WriteLine($"Символов в элементе будет не более: {K}");
+Console.WriteLine($"\nЭлементов в итоговом наборе: {calk}");
 
 Console.Write("\nИтоговый список: ");
 Console.WriteLine(String.Join(", ", myArray2));
